e046f74d00b7fe76e487e66982cb17036d1a1ab9
Kyle Aoki <vk1997489@gmail.com>
2020-11-17 09:02:39 -0800

    adds the request batch card as well as its successmodal

--- src/views/HomePage/HomePage.tsx
+++ src/views/HomePage/HomePage.tsx
@@ -4,6 +4,8 @@ import { BatchCard } from '../../components/BatchCard/BatchCard';
 import { NavBar } from '../../components/NavBar/NavBar';
 import reactReduxLogo from '../../assets/react-redux-logo.png';
 import javaLogo from '../../assets/java-logo.png';
+import RequestBatchCard from '../../components/RequestBatchCard/RequestBatchCard'
+import RequestBatchCardModal from '../../components/RequestBatchCard/RequestBatchCardModal'
 
 export const HomePage:React.FC<undefined> = () => {
 
@@ -12,6 +14,8 @@ export const HomePage:React.FC<undefined> = () => {
     return(
         <Container style={{height: "100vh", maxWidth: "100vw", backgroundColor:"#B9B9BA"}}>
             <NavBar />
+            <RequestBatchCard />
+            <RequestBatchCardModal />
             <Row className="justify-content-between">
                 <Col xs="auto">
                 </Col>
@@ -22,7 +26,7 @@ export const HomePage:React.FC<undefined> = () => {
                     <p style={{color: "#474C55", fontSize:"1.2em"}}>Click to find out more...</p>
                     <br />
                     <Container>
-                        <Row>
+                        {/* <Row>
                             <Col xl="2" lg="3" md="4" sm="4" xs="6">
                                 <BatchCard titlePic={reactReduxLogo} specialization="React-Redux" batchName="Batch Name" />
                             </Col>
@@ -32,7 +36,7 @@ export const HomePage:React.FC<undefined> = () => {
                             <Col xl="2" lg="3" md="4" sm="4" xs="6">
                                 <BatchCard titlePic={reactReduxLogo} specialization="React-Redux" batchName="Batch Name" />
                             </Col>
-                        </Row>
+                        </Row> */}
                     </Container>
                 </Col>
                 <Col xs="auto">
