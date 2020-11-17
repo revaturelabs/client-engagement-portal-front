f4ce9f7804502052ca1b28ae3dc7494c896da0b6
sleepygibbs <gibbs041398@gmail.com>
2020-11-17 11:57:41 -0500

    creating the select input form

--- src/views/AdminPage/AdminPage.tsx
+++ src/views/AdminPage/AdminPage.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Col, Container, Form, Input, Row } from 'reactstrap';
 import {NavBar} from '../../components/NavBar/NavBar'
 
 
 export const AdminPage: React.FC = () => {
     return (
-        <div>
+        <Container style={{height: "100vh", maxWidth: "100vw", backgroundColor:"#B9B9BA"}}>
             <NavBar />
-        </div>
+            <Row className="justify-content-between">
+                <Col>
+                    <div>
+                        <Form>
+                            <Input type="select">Select Client
+                                <option>Client 1</option>
+                                <option>Client 2</option>
+                            </Input>
+                            <Input type="select">Select Batch
+                                <option>Batch 206</option>
+                                <option>Batch 207</option>
+                            </Input>
+                        </Form>
+                    </div>
+                </Col>
+                <Col></Col>
+            </Row>
+        </Container>
     )
 }  
