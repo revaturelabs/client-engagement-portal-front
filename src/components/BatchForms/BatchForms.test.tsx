cb49f056f2bb79b46b9ad3960a1e7c27e3d142f8
sleepygibbs <gibbs041398@gmail.com>
2020-11-20 16:36:06 -0500

    commited tests

--- src/components/BatchForms/BatchForms.test.tsx
+++ src/components/BatchForms/BatchForms.test.tsx
@@ -7,15 +7,32 @@ import axios from 'axios';
 Enzyme.configure({adapter: new Adapter() });
 const wrapper=shallow(<BatchForms/>);
 
+
+/**
+ * @function
+ * Testing the batch forms
+ */
 describe('BatchForms',()=>{
+    /**
+     * @function
+     * testing to find the that the map batch button is rendering with correct text
+     */
     it('should find map header on button',()=>{
         const text = wrapper.find('#map-batch');
         expect(text.text()).toBe('Map Batch To Client');
     });
+    /**
+     * @function
+     * testing to find the that the unmap batch button is rendering with correct text
+     */
     it('should find unmap header on button',()=>{
         const text = wrapper.find('#unmap-batch');
         expect(text.text()).toBe('Unmap Batch From Client');
     });
+    /**
+     * @function
+     * testing to find ensure we have options
+     */
     it('should populate select batch options',()=>{
         const options = wrapper.find('#map-options')
         expect(options.length).toBeGreaterThan(0);
