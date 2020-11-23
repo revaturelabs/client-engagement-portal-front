4873a7c485639179836f9d25f1bb68bca9aa837e
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-11-23 12:03:49 -0800

    More cleanup

--- src/components/RouterGuard/RouterGuard.tsx
+++ src/components/RouterGuard/RouterGuard.tsx
@@ -5,39 +5,42 @@ import { Redirect, Route, RouteProps } from "react-router";
 
 //Props for RouterGuard. Expected to be in a BrowserRouter Switch, so extends RouteProps
 export interface ProtectedRouteProps extends RouteProps {
-  role: string[]; //allowed role
+  role: string[]; //allowed roles
   redirectPath: string; //path to redirect to if fail
 }
 
 export const RouterGuard: React.FC<ProtectedRouteProps> = (props) => {
   const [redirect, setRedirect] = useState<string>("");
-  const [userRole, setUserRole] = useState<string>("");       // if not yet loaded (Auth not done yet), then falsy
+  const [userRole, setUserRole] = useState<string>(""); // if not yet loaded (Auth not done yet), then falsy
   // const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     async function getUser() {
-      const userInfo = await Auth.currentUserInfo();    //get current user
+      const userInfo = await Auth.currentUserInfo(); //get current user
       const user = userInfo?.attributes["custom:userRole"] || "NotSignedIn";
-      
-      if (userInfo && !props.role.includes(user)) {      // if the current user role is not one of the allowed roles, then redirect
+
+      if (userInfo && !props.role.includes(user)) {
+        // if the current user role is not one of the allowed roles, then redirect
         setRedirect(props.redirectPath);
         // console.log("redirecting");
       }
-      setUserRole(user);                  //set UserRole to be the found user. We know it's fine bec of await.
+      setUserRole(user); //set UserRole to be the found user. We know it's fine bec of await.
     }
-    !userRole && getUser();    //run only if not yet loaded
+    !userRole && getUser(); //run only if not yet loaded
   });
-  
-  
 
-  if (userRole) {                                         //If the page is loaded...
-    if (redirect || userRole === "NotSignedIn") {                                     //if the user role is not correct, return redirect
+  if (userRole) {
+    //If the page is loaded...
+    if (redirect || userRole === "NotSignedIn") {
+      //if the user role is not correct, return redirect
       const renderComponent = () => <Redirect to={{ pathname: redirect }} />;
       return <Route {...props} component={renderComponent} render={undefined} />;
-    } else {                                                //else return route normally
+    } else {
+      //else return route normally
       return <Route {...props} />;
     }
-  } else {                                              //else don't load
+  } else {
+    //else don't load
     return null;
   }
 };
