1d365c6702ef8851466aa4470df384e62e320405
hernandezn <hernandezn@live.com>
2020-11-22 21:38:24 -0500

    downgrading to enzyme-compatible react version

---
+++ src/components/BatchCard/BatchCard.test.tsx
@@ -0,0 +1,23 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import { BatchCard } from './BatchCard';
+
+const batch = {
+    batchId: 5,
+    specialization: "None",
+    batchName: "A Mock Batch"
+};
+
+describe("Batch Card ", () => {
+    it("accepts batch props", () => {
+        const wrapper = mount(<BatchCard {... batch} />);
+
+        expect(wrapper.props()).toEqual(batch);
+    });
+
+    // it("contains specialization text", () => {
+    //     const wrapper = mount(<BatchCard {... batch} />);
+    //     const receivedText = wrapper.find("p.spec-text").text();
+    //     expect(receivedText).toEqual("SalesForce");
+    // })
+});
