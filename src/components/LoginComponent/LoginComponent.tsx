5136e1630c584742a7b5bb21d503ac7d960c5bba
Dawit Wondim <dawit346@gmail.com>
2020-11-23 11:24:46 -0800

    Made the updates to login component

--- src/components/LoginComponent/LoginComponent.tsx
+++ src/components/LoginComponent/LoginComponent.tsx
@@ -65,25 +65,6 @@ export const LoginComponent: React.FC<ILoginProps> = (props: ILoginProps) => {
 
     }
 
-<<<<<<< HEAD
-    return(
-            <>
-            {isValidated ? <Redirect to="/home" /> :
-                <form onSubmit={handleSubmit}
-                    style={{textAlign: "center", backgroundColor: "white", width: "15vw", height: "32vh", minWidth:"200px", display: "inline-block",
-                            borderRadius: "50px", padding: "10px", border: "1px solid #F26925"}}>
-                
-                    <div style={{maxHeight: "90%"}}>
-                        <div style={{position: "relative", textAlign: "center"}}>
-                            <Link to="/login"><img src={hands} alt="hands" style={{width: "60%", minHeight: "6em", opacity: 0.1}} /></Link>
-                            <div className="logoarea"
-                                style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} >
-
-                                <div
-                                    className = "test1" style={{ color: "#202430", fontSize: "20px", fontWeight:500 }}>    
-                                    Engagement Force
-                                </div>
-=======
     return (
         <>
             {isClient ? <Redirect to="/home" /> : isAdmin ? <Redirect to="/admin" /> :
@@ -96,41 +77,20 @@ export const LoginComponent: React.FC<ILoginProps> = (props: ILoginProps) => {
                         </div>
                         <div className="cep-logo-area">
                             <img src={ceplogo2} alt="cep-logo" width="200px"/>
->>>>>>> 72a8fc8243ed44954d401029a1db8e8afbff3f81
                             </div>
                         </div>
 
                         <div style={{ position: "relative" }}>
                             <input type="email" required className="form-control" name="email" placeholder="E-mail"
-<<<<<<< HEAD
-                                style={new CEPLoginInputStyle()}/>
-                            <div style={{position: "absolute", top: "45%", left: "21%", transform: "translate(-50%, -50%)"}}>
-                                <img src={userThumb} alt="email-thumbnail" className="userthumbcheck"/>
-=======
                                 style={new CEPLoginInputStyle()} />
                             <div style={{ position: "absolute", top: "45%", left: "21%", transform: "translate(-50%, -50%)" }}>
                                 <img src={userThumb} alt="email thumbnail" className="userthumbcheck" />
->>>>>>> 72a8fc8243ed44954d401029a1db8e8afbff3f81
                             </div>
                         </div>
 
                         <div style={{ position: "relative" }}>
                             <input type="password" required className="form-control" name="password" placeholder="Password"
                                 style={new CEPLoginInputStyle()} />
-<<<<<<< HEAD
-                            <div style={{position: "absolute", top: "45%", left: "21%", transform: "translate(-50%, -50%)"}}>
-                                <img src={passThumb} alt="password-thumbnail" className= "passthumbcheck" />
-                            </div>
-                        </div>
-    
-                        <button className="test2" type="submit"
-                            style={{margin: "10px", lineHeight: 2.2, width: "70%", border:"none", backgroundColor:"#F26925", color:"white", fontSize:"20px"}}>Login</button>
-                    </div>
-                </form>
-            }
-            </>
-      );
-=======
                             <div style={{ position: "absolute", top: "45%", left: "21%", transform: "translate(-50%, -50%)" }}>
                                 <img src={passThumb} alt="password thumbnail" className= "passthumbcheck" />
                             </div>
@@ -146,7 +106,6 @@ export const LoginComponent: React.FC<ILoginProps> = (props: ILoginProps) => {
             }
         </>
     );
->>>>>>> 72a8fc8243ed44954d401029a1db8e8afbff3f81
 }
 
 export class CEPLoginInputStyle implements React.CSSProperties {
