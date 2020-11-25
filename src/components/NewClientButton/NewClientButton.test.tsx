c35544a22b0502ac1d1552c483640df376d4016c
ar-pearse <andrew.roy.pearse@gmail.com>
2020-11-25 10:14:11 -0600

    Removed/Fixed some tests

--- src/components/NewClientButton/NewClientButton.test.tsx
+++ src/components/NewClientButton/NewClientButton.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { NewClientButton } from "./NewClientButton";
-import { Label, Input } from "reactstrap";
+import { Label, Modal, Input } from "reactstrap";
 
 let wrapper: any;
 
@@ -9,12 +9,22 @@ beforeEach(() => {
   wrapper = shallow(<NewClientButton />);
 });
 
+test("modal toggles on and off", () => {
+  expect(wrapper.find(Modal).prop("isOpen")).toBe(false);
+  wrapper.find(".toggleButton").simulate("click");
+  expect(wrapper.find(Modal).prop("isOpen")).toBe(true);
+});
+
 it("modal should have 5 label fields", () => {
   const labels = wrapper.find(Label);
-  expect(labels.length).toBe(6);
+  expect(labels.length).toBe(5);
 });
 
 it("modal should have 5 input fields", () => {
   const inputs = wrapper.find(Input);
-  expect(inputs.length).toBe(6);
+  expect(inputs.length).toBe(5);
+});
+
+it("submit button should say 'Submit'", () => {
+  expect(wrapper.find(".newClientButton").render().text()).toBe("Submit");
 });
