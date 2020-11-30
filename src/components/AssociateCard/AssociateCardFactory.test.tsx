46fc461a5dc0f4f124b319260b3b7f7720393f0e
JordanHunnicutt <jordanhunnicutt97@gmail.com>
2020-11-30 09:56:35 -0500

    comments on factory test

--- src/components/AssociateCard/AssociateCardFactory.test.tsx
+++ src/components/AssociateCard/AssociateCardFactory.test.tsx
@@ -49,12 +49,15 @@ beforeEach(() => {
     wrapper = shallow(<AssociateCardFactory />);
 });
 
+/**
+ * this test ensures that no cards are displayed when to info is passed in as props
+ */
 test("content is empty without any batch information given", () => {
     const contentText = wrapper.find(".holderCarousel").render().text();
     expect(contentText).toBe("");
 });
 /**
- * this isn't truthy, need to look into it
+ * this test inserts data into the cards variable. Due to how shallow works, no content is displayed in contentText, but the branch is tested.
  */
 test("cards is filled with data when the dactory is provided data. the text of content is still falsy due to how shallow works", () => {
     wrapper = shallow(<AssociateCardFactory {...fakeData}/>);
