33224375c24b92de3169882c4229aef62036fcd5
Eterreri <ejterreri@gmail.com>
2021-01-15 15:27:37 -0800

    Added backend connection to new Homepage

--- src/views/OmePage/api.ts
+++ src/views/OmePage/api.ts
@@ -1,19 +1,64 @@
-import { Batch } from './types';
-import dummyBatches from './dummyBatches.json';
+import { Batch } from "./types";
+import { axiosInstance } from "../../util/axiosConfig";
 
-export const getDummyBatches = async (): Promise<Batch[]> => {
-  await new Promise(res => setTimeout(res, 500));
-  return dummyBatches as any as Batch[];
-}
+export const getAdminBatches = async (): Promise<Batch[]> => {
+  let batches = [] as Batch[];
+  await axiosInstance().then((result) => {
+    result
+      .get("/admin/batches")
+      .then((response: any) => {
+        if (response != null) {
+          //individual batch info is placed into the array from above
+          for (const batchData of response.data) {
+            const batchCardInfo = { ...batchData };
+            batches.push(batchCardInfo);
+          }
+        }
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  });
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  return batches as Batch[];
+};
 
 export const getClientBatches = async (userEmail: String): Promise<Batch[]> => {
-  await new Promise(res => setTimeout(res, 500));
-  return [dummyBatches[1],dummyBatches[2]] as any as Batch[];
-}
+  let batches = [] as Batch[];
+  await axiosInstance().then((result) => {
+    result
+      .get("/client/email/batch/" + userEmail)
+      .then((response: any) => {
+        if (response != null) {
+          //individual batch info is placed into the array from above
+          for (const batchData of response.data) {
+            const batchCardInfo = { ...batchData };
+            batches.push(batchCardInfo);
+          }
+        }
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  });
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  return batches as Batch[];
+};
 
-export const getSingleBatch = async (batchId: string):
-    Promise<Batch | undefined> => {
-  await new Promise(res => setTimeout(res, 500));
-  const batches = dummyBatches as any as Batch[];
-  return batches.find(b => b.batchId === batchId);
-}
+export const getSingleBatch = async (batchId: string): Promise<Batch | undefined> => {
+  let batch;
+  await axiosInstance().then((result) => {
+    result
+      .get("/client/batch/" + batchId)
+      .then((response: any) => {
+        if (response != null) {
+          batch = response.data;
+        }
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  });
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  return batch as any as Batch;
+};
