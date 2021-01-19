282bd545b925734bcb4e3dd3e505d2d0fd5de6f6
ACAPP-dev <acapp909@gmail.com>
2021-01-19 14:55:52 -0800

    update graph styling

--- src/views/BatchInfoPage/BatchInfoPageContent.tsx
+++ src/views/BatchInfoPage/BatchInfoPageContent.tsx
@@ -1,55 +1,54 @@
-import { Batch } from '../../types';
-import React from 'react';
-import { Col, Container, DropdownItem, Row } from 'reactstrap';
-import { NavBar } from '../../components/NavBar/NavBar';
-import { Link } from 'react-router-dom';
-import '../../scss/batch-info-page-content.scss';
-import { User } from '../../types';
-import BatchCardLarge from '../../components/BatchCardLarge/BatchCardLarge';
-import { AssociateCard } from '../../components/AssociateCard/AssociateCard';
-import Notifications from '../../components/Notifications/Notifications';
+import { Batch } from "../../types";
+import React from "react";
+import { Col, Container, DropdownItem, Row } from "reactstrap";
+import { NavBar } from "../../components/NavBar/NavBar";
+import { Link } from "react-router-dom";
+import "../../scss/batch-info-page-content.scss";
+import { User } from "../../types";
+import BatchCardLarge from "../../components/BatchCardLarge/BatchCardLarge";
+import { AssociateCard } from "../../components/AssociateCard/AssociateCard";
+import Notifications from "../../components/Notifications/Notifications";
+import BatchAverageGraph from "../../components/Graphs/BatchAverageGraph";
 
 interface IBatchInfoPageContentProps {
   batch: Batch;
   user: User;
 }
 
-const BatchInfoPageContent: React.FC<IBatchInfoPageContentProps> = props => {
+const BatchInfoPageContent: React.FC<IBatchInfoPageContentProps> = (props) => {
   return (
-      <Container className='batch-info-page-content' fluid>
-        <NavBar>
-          <Link to='/home'>
-            <DropdownItem>Home</DropdownItem>
-          </Link>
+    <Container className="batch-info-page-content" fluid>
+      <NavBar>
+        <Link to="/home">
+          <DropdownItem>Home</DropdownItem>
+        </Link>
 
-          {
-            props.user.role === 'admin' &&
-            (
-                <Link to='/admin'>
-                  <DropdownItem>Map Clients</DropdownItem>
-                </Link>
-            )
-          }
-        </NavBar>
-        <Row className='justify-content-center'>
-          <Col sm='1'/>
-          <Col sm='10' md='8' xl='5'>
-            <div id='batch-info-wrapper'>
-              <BatchCardLarge batch={ props.batch } user={props.user}/>
-              {
-                props.batch.associateAssignments.map((a, index) =>
-                    ( <AssociateCard key={index} associateAssignment={a}/> )
-                )
-              }
-              {
-                props.user.role === 'admin' &&
-                <Notifications/>
-              }
+        {props.user.role === "admin" && (
+          <Link to="/admin">
+            <DropdownItem>Map Clients</DropdownItem>
+          </Link>
+        )}
+      </NavBar>
+      <Row className="justify-content-center">
+        <Col sm="1" />
+        <Col sm="10" md="8" xl="5">
+          <div id="batch-info-wrapper">
+            <BatchCardLarge batch={props.batch} user={props.user} />
+            <div
+              className="batch-info-card"
+              style={{ background: "white", marginTop: "10px" }}
+            >
+              <BatchAverageGraph batch={props.batch} />
             </div>
-          </Col>
-          <Col xs='1'/>
-        </Row>
-      </Container>
+            {props.batch.associateAssignments.map((a, index) => (
+              <AssociateCard key={index} associateAssignment={a} />
+            ))}
+            {props.user.role === "admin" && <Notifications />}
+          </div>
+        </Col>
+        <Col xs="1" />
+      </Row>
+    </Container>
   );
 };
 
