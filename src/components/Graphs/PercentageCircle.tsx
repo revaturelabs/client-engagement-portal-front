6df2cc32591dd39d5eb8c32a0a31581f1bea6293
nickBarak <nicholasjbarak@gmail.com>
2021-01-19 07:33:54 -0800

    new styling, not responsive

--- src/components/Graphs/PercentageCircle.tsx
+++ src/components/Graphs/PercentageCircle.tsx
@@ -3,25 +3,98 @@ import { Batch } from '../../types';
 import { findAverage } from '../../util';
 
 const PercentageCircle : React.FC<{batch:Batch}> = ({ batch }) => {
-    const [integer, decimals] = findAverage(
+    const percentage = (findAverage(
         batch.associateAssignments
             .filter(({ associate: {grades} }) => grades)
             // Grades are assured to exist after filter
             .map( ({ associate: {grades} }) => findAverage(grades!.map(({ score }) => score)) )
-    ).toFixed(2).split('.');
+    ) / 100).toFixed(4)
+        || .000;
 
     return (
-        <div className="circles-wrp" style={{ "position": "relative", "display": "inline-block" }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80">
-                <path fill="transparent" stroke="#D3B6C6" stroke-width="7" d="M 39.992565928065126 3.500000757060633 A 36.5 36.5 0 1 1 39.949302232260386 3.500035209108127 Z"></path>
-                <path fill="transparent" stroke="#4B253A" stroke-width="7" d="M 39.992565928065126 3.500000757060633 A 36.5 36.5 0 0 1 59.07174573163989 71.12103010421852 "></path>
-            </svg>
-            <div className="circles-text" style={{ "position": "absolute", "top": "0px", "left": "0px", "textAlign": "center", "width": "100%", "fontSize": "28px", "height": "80px", "lineHeight": "80px" }}>
-                <span className="circles-integer">{integer}</span>.
-                <span className="circles-decimals">{decimals}%</span>
+         <div className="percentage-circle-container">
+             <div className="percentage-circle">
+                <svg>
+                    <circle cx="70" cy="70" r="50" />
+                    <circle cx="70" cy="70" r="50" />
+                </svg>
+                <div className="percentage-circle-number">
+                    <h2>{(+percentage * 100).toFixed(2)}<span>%</span></h2>
+                </div>
             </div>
-        </div>
+            <h2  className="percentage-circle-title">Batch Average Grade</h2>
 
+            <style>{`
+                .percentage-circle-container {
+                    position: relative;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    flex-direction: column;
+                    height: 150px;
+                    width: 150px;
+                }
+
+                .percentage-circle-container .percentage-circle {
+                    position: relative;
+                }
+
+                .percentage-circle-container .percentage-circle svg {
+                    position: relative;
+                    width: 150px;
+                    height: 150px;
+                    transform: rotateZ(-90deg);
+                }
+
+                .percentage-circle-container .percentage-circle svg circle {
+                    fill: none;
+                    stroke-width: 10;
+                    stroke: #000;
+                    transform: translate(5px, 5px);
+                    stroke-dasharray: 314;
+                    stroke-dashoffset: 314;
+                    stroke-linecap: round;
+                }
+
+                .percentage-circle-container .percentage-circle svg circle:nth-child(1) {
+                    stroke-dashoffset: 0;
+                    stroke: #f3f3f3;
+                }
+
+                .percentage-circle-container .percentage-circle svg circle:nth-child(2) {
+                    stroke-dashoffset: ${314 * (1 - +percentage)};
+                    stroke: #03a9f4;
+                }
+
+                .percentage-circle-container .percentage-circle .percentage-circle-number {
+                    position: absolute;
+                    top: .42rem;
+                    left: .1rem;
+                    width: 100%;
+                    height: 100%;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    color: #999;
+                }
+
+                .percentage-circle-container .percentage-circle .percentage-circle-number h2 {
+                    font-size: 1.25rem;
+                }
+
+                .percentage-circle-container .percentage-circle .percentage-circle-number h2 span {
+                    font-size: .8rem;
+                }
+
+                .percentage-circle-container .percentage-circle-title {
+                    color: #999;
+                    font-weight: 700;
+                    letter-spacing: 1px;
+                    font-size: 1.025rem;
+                    text-align: center;
+                }
+            `}</style>
+        </div>
     )
 }
 
