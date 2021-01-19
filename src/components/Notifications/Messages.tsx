0d483473350dbc2d7e04a58e528504b7fe35ab7d
tkmoney11 <tylerkim@outlook.com>
2021-01-19 11:06:39 -0800

    Nearly fully integrated modals

--- src/components/Notifications/Messages.tsx
+++ src/components/Notifications/Messages.tsx
@@ -1,81 +1,103 @@
-import React, { useState } from 'react';
-import { Accordion, Card } from 'react-bootstrap';
-import { connect, useDispatch } from 'react-redux';
-import { axiosInstance } from '../../util/axiosConfig';
-import { IRootState } from '../../_reducers';
-import { IMessageState } from '../../_reducers/MessagesReducer';
-import { setMessages } from '../../actions/MessageActions';
-import '../../scss/Notifications.scss';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { Accordion, Card } from "react-bootstrap";
+import { connect, useDispatch } from "react-redux";
+import { axiosInstance } from "../../util/axiosConfig";
+import { IRootState } from "../../_reducers";
+import { IMessageState } from "../../_reducers/MessagesReducer";
+import { setMessages } from "../../actions/MessageActions";
+import "../../scss/Notifications.scss";
+import { useSelector } from "react-redux";
+import { ReplyMessageModal } from "./../MessagesModals/ReplyModal";
 
-const Messages:React.FC<IMessageState> = (props:IMessageState) => {
+const Messages: React.FC<IMessageState> = (props: IMessageState) => {
+  const [showReply, setShowReply] = useState(false);
+  const toggleReply = () => setShowReply(!showReply);
+  const [requests, gotRequests] = useState(false);
+  const dispatch = useDispatch();
 
-    const [requests, gotRequests] = useState(false);
-    const dispatch = useDispatch();
+  let userEmail = useSelector((state: IRootState) => {
+    var email = `${state.userState.user?.email}`;
+    return email;
+  });
 
-    let userEmail = useSelector((state: IRootState) => {
-        var email = `${state.userState.user?.email}`
-        return email
-    });
+  const getMessages = async () => {
+    var URL = getUrl();
+    const response = await (await axiosInstance()).get(URL);
+    dispatch(setMessages(response.data));
+    gotRequests(true);
+  };
 
-   const getMessages = async () => {
-       var URL = getUrl()
-       const response = await (await axiosInstance()).get(URL);
-       dispatch(setMessages(response.data));
-       gotRequests(true);
-   }
-
-    const getUrl = () =>{
-        const URL = "message/" + userEmail
-        return URL
+  const getUrl = () => {
+    const URL = "message/" + userEmail;
+    return URL;
+  };
+  function sender(isAdmin: boolean, adminEmail: string, clientEmail: string) {
+    if (isAdmin) {
+      return adminEmail;
+    } else {
+      return clientEmail;
     }
-    function sender (isAdmin: boolean, adminEmail: string, clientEmail: string){
-        if(isAdmin){
-            return adminEmail
-        }
-        else{
-            return clientEmail
-        }
-    }
+  }
 
-   if(!requests) {
-       getMessages();
-   }
-   return(
+  if (!requests) {
+    getMessages();
+  }
+  return (
     <>
-    {props.messages?.map(
-        (e, i) =>
+      {props.messages?.map((e, i) => (
         <>
-            <div className="card border-dark">
-                <div className="card-body">
-                    <h5 className="message-title">Sender: {sender(e.adminTheSender, e.adminId.email, e.clientId.email)}</h5>
+          <ReplyMessageModal
+            show={showReply}
+            toggle={() => toggleReply()}
+            title={e.title}
+            recipient={e.clientId.email}
+          />
+          <div className="card border-dark">
+            <div className="card-body">
+              <h5 className="message-title">
+                Sender:{" "}
+                {sender(e.adminTheSender, e.adminId.email, e.clientId.email)}
+              </h5>
 
-                    <div className="row">
-                        <h6><div className='col-md'>Date: {e.dateSent}</div></h6>
-                        <h6><div className='col-md'>Company Name: {e.clientId.companyName}</div> </h6>
-                    </div>
+              <div className="row">
+                <h6>
+                  <div className="col-md">Date: {e.dateSent}</div>
+                </h6>
+                <h6>
+                  <div className="col-md">
+                    Company Name: {e.clientId.companyName}
+                  </div>{" "}
+                </h6>
+              </div>
 
-                    <div className="row">
-                        <p className="card-text col-md">{e.message}</p>
-                        <div className="col-md">
-                            <a href="#" className="btn btn-primary mr-2 float-right">reply</a> 
-                            <a href="#" className="btn btn-primary mr-2 float-right">delete</a>
-                        </div>
-                    </div>
+              <div className="row">
+                <p className="card-text col-md">{e.message}</p>
+                <div className="col-md">
+                  <button
+                    onClick={toggleReply}
+                    className="btn btn-primary mr-2 float-right"
+                  >
+                    reply
+                  </button>
+                  <a href="#" className="btn btn-primary mr-2 float-right">
+                    delete
+                  </a>
                 </div>
+              </div>
             </div>
-        <br></br>
-        </>        
-    )}
+          </div>
+          <br></br>
+        </>
+      ))}
     </>
-   )
-}
+  );
+};
 
-const mapStateToProps = (state:IRootState) => {
-    return {
-        messages: state.messageState.messages
-    }
-}
+const mapStateToProps = (state: IRootState) => {
+  return {
+    messages: state.messageState.messages,
+  };
+};
 
 /**
  * Sends the current notification state to the Notification component.
