9810ff90ec33d2e0b916c815829acbad7cb05007
tkmoney11 <tylerkim@outlook.com>
2021-01-19 17:11:49 -0800

    Removed errors

--- src/components/PlanInterventionModal/PlanInterventionModalv2.tsx
+++ src/components/PlanInterventionModal/PlanInterventionModalv2.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import { Form, FormGroup, Input, Label, Modal, ModalBody, ModalHeader, Row } from 'reactstrap';
-import './PlanInterventionModal.scss';
-import { useSelector } from 'react-redux';
-import { IRootState } from '../../_reducers';
-import { axiosInstance } from '../../util/axiosConfig';
-import emailjs from 'emailjs-com';
-
+import React from "react";
+import {
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Row,
+} from "reactstrap";
+import "./PlanInterventionModal.scss";
+import { useSelector } from "react-redux";
+import { IRootState } from "../../_reducers";
+import { axiosInstance } from "../../util/axiosConfig";
+import emailjs from "emailjs-com";
 
 /**
  * @function PlanInterventionModalv2
@@ -13,143 +21,163 @@ import emailjs from 'emailjs-com';
  * when the button is clicked.
  */
 const PlanInterventionModalv2: React.FC = () => {
+  const [show, setShow] = React.useState(false);
+  const [clientInfo, setClientInfo] = React.useState({});
+  const toggle = () => setShow(!show);
 
-    const [show, setShow] = React.useState(false);
-    const [clientInfo, setClientInfo] = React.useState({});
-    const toggle = () => setShow(!show);
+  /**
+   * sets clientEmail equal to the email of the client that is logged in
+   */
+  let clientEmail = useSelector((state: IRootState) => {
+    return `${state.userState.user?.email}`;
+  });
 
-    /**
-       * sets clientEmail equal to the email of the client that is logged in
-       */
-    let clientEmail = useSelector((state: IRootState) => {
-        return `${state.userState.user?.email}`
-    });
-    let clientFirstName = useSelector((state: IRootState) => {
-        return `${state.userState.user?.firstName}`
-    })
-    let clientLastName = useSelector((state: IRootState) => {
-        return `${state.userState.user?.lastName}`
-    })
+  /**
+   * @Function requestTalentFormSubmit
+   * Send form data to the backend to request more talent
+   *
+   * @param event
+   * Collecting information from the form
+   */
+  const requestInterventionFormSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
 
-    /**
-      * @Function requestTalentFormSubmit
-      * Send form data to the backend to request more talent
-      * 
-      * @param event 
-      * Collecting information from the form
-      */
-    const requestInterventionFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        const message = event.currentTarget["message"].value;
-        const requestType = "INTERVENTION";
-        try {
-            (await axiosInstance()).post(`intervention/`, {
-                clientEmail: clientEmail,
-                firstName: clientFirstName,
-                lastName: clientLastName,
-                message: message,
-                requestId: 0,
-                requestType: requestType,
-                status: "PENDING",
-            }).then((response) => {
-                const data = response
-                console.log("data ", data.config.data)
-                setClientInfo(data.config.data);
-                console.log('data2 ', clientInfo)
-
-            });
-        }
-        catch (error) {
-            return false;
-        }
-        toggle();
-        alert("Request Received!")
-        return true;
-    };
-    
-    
-
-
-    var adminList:Number[] = [];
-    var adminEmailList:String[] = [];
-    var ClientId:Number;
-    var Message:String;
-    async function NoticeAdmin() {
-        Message = (document.getElementById("message") as HTMLInputElement).value;
-        console.log("You get a notification from Client: ", '\n', " Message: ", Message);
-        (await axiosInstance()).get(`client/email/${clientEmail}`).then((response) => {
-            const data = response
-            console.log("clientinfo ", data)
-            ClientId = data.data.clientId;
-            
+    const message = event.currentTarget["message"].value;
+    const requestType = "INTERVENTION";
+    try {
+      (await axiosInstance())
+        .post(`intervention/`, {
+          clientEmail: clientEmail,
+          message: message,
+          requestId: 0,
+          requestType: requestType,
+          status: "PENDING",
+        })
+        .then((response) => {
+          const data = response;
+          console.log("data ", data.config.data);
+          setClientInfo(data.config.data);
+          console.log("data2 ", clientInfo);
         });
-       
-        
-        (await axiosInstance()).get(`admin/`).then((response) => {
-            const data = response
-            for (let i = 0; i < data.data.length; i++) {
-                console.log("adminEmail: ", data.data[i].email);
-                adminEmailList.push(data.data[i].email)
-                adminList.push(data.data[i].adminId)
-            }
-
-            for (let email of adminEmailList){
-                console.log(adminList);
-                let mailOptions = {
-                    firstName: clientFirstName,
-                    lastName: clientLastName,
-                    adminEmail: email,
-                    message: Message
-                };
-                emailjs.send('service_780easr', 'template_1co3ggw', mailOptions, 'user_qNzha4WnrQ4xZeolBYZkl')
-                    .then((result) => {
-                        console.log(result.text);
-                    }, (error) => {
-                        console.log(error.text);
-                    });     
-            };
-            postRequestMsg();
-        });
+    } catch (error) {
+      return false;
     }
-    
-    async function postRequestMsg() {
-        for (let adminId2 of adminList){
-            (await axiosInstance()).post(`msg/client`,{
-                adminId : adminId2,
-                clientId: ClientId,
-                message: Message
-            }).then((res)=>{
-                console.log(res);
-            });
-            console.log("idminId", adminId2)
-            console.log("clientId", ClientId)
-            console.log("imessage", Message);
-        }
-   
-}
+    toggle();
+    alert("Request Received!");
+    return true;
+  };
 
-    return (
-
-        <>
-
-            <button onClick={toggle} className="intervention-button">Request Intervention</button>
-            <Modal isOpen={show} toggle={toggle}>
-                <ModalHeader toggle={toggle}>
-                    <h4>Request Intervention</h4>
-                </ModalHeader>
-                <ModalBody style={{ textAlign: "center" }}>
-                    <Form onSubmit={(event: React.FormEvent<HTMLFormElement>) => requestInterventionFormSubmit(event)}>
-                        <FormGroup>
-                            <Label for="message" className="TextAreaLabel">Message:</Label>
-                            <Input type="textarea" name="message" id="message" className="TextAreaInput" placeholder="Reason for intervention, target dates/times, etc..."></Input>
-                            <input type="submit" value="Submit" className="talentSubmit" onClick={NoticeAdmin}></input>
-                        </FormGroup>
-                    </Form>
-                </ModalBody>
-            </Modal>
-        </>
+  var adminList: Number[] = [];
+  var adminEmailList: String[] = [];
+  var ClientId: Number;
+  var Message: String;
+  async function NoticeAdmin() {
+    Message = (document.getElementById("message") as HTMLInputElement).value;
+    console.log(
+      "You get a notification from Client: ",
+      "\n",
+      " Message: ",
+      Message
     );
-}
+    (await axiosInstance())
+      .get(`client/email/${clientEmail}`)
+      .then((response) => {
+        const data = response;
+        console.log("clientinfo ", data);
+        ClientId = data.data.clientId;
+      });
+
+    (await axiosInstance()).get(`admin/`).then((response) => {
+      const data = response;
+      for (let i = 0; i < data.data.length; i++) {
+        console.log("adminEmail: ", data.data[i].email);
+        adminEmailList.push(data.data[i].email);
+        adminList.push(data.data[i].adminId);
+      }
+
+      for (let email of adminEmailList) {
+        console.log(adminList);
+        let mailOptions = {
+          adminEmail: email,
+          message: Message,
+        };
+        emailjs
+          .send(
+            "service_780easr",
+            "template_1co3ggw",
+            mailOptions,
+            "user_qNzha4WnrQ4xZeolBYZkl"
+          )
+          .then(
+            (result) => {
+              console.log(result.text);
+            },
+            (error) => {
+              console.log(error.text);
+            }
+          );
+      }
+      postRequestMsg();
+    });
+  }
+
+  async function postRequestMsg() {
+    for (let adminId2 of adminList) {
+      (await axiosInstance())
+        .post(`msg/client`, {
+          adminId: adminId2,
+          clientId: ClientId,
+          message: Message,
+        })
+        .then((res) => {
+          console.log(res);
+        });
+      console.log("idminId", adminId2);
+      console.log("clientId", ClientId);
+      console.log("imessage", Message);
+    }
+  }
+
+  return (
+    <>
+      <button onClick={toggle} className="intervention-button">
+        Request Intervention
+      </button>
+      <Modal isOpen={show} toggle={toggle}>
+        <ModalHeader toggle={toggle}>
+          <h4>Request Intervention</h4>
+        </ModalHeader>
+        <ModalBody style={{ textAlign: "center" }}>
+          <Form
+            onSubmit={(event: React.FormEvent<HTMLFormElement>) =>
+              requestInterventionFormSubmit(event)
+            }
+          >
+            <FormGroup>
+              <Label for="message" className="TextAreaLabel">
+                Message:
+              </Label>
+              <Input
+                type="textarea"
+                name="message"
+                id="message"
+                className="TextAreaInput"
+                placeholder="Reason for intervention, target dates/times, etc..."
+              ></Input>
+              <input
+                type="submit"
+                value="Submit"
+                className="talentSubmit"
+                onClick={NoticeAdmin}
+              ></input>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+};
 
 export default PlanInterventionModalv2;
--- src/components/RequestTalentModal/RequestTalent.tsx
+++ src/components/RequestTalentModal/RequestTalent.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Form, FormGroup, Input, Label, Modal, ModalBody, ModalHeader, Row } from 'reactstrap';
-import './RequestTalent.scss'
-import { useSelector } from 'react-redux';
-import { IRootState } from '../../_reducers';
-import { axiosInstance } from '../../util/axiosConfig';
-import emailjs from 'emailjs-com';
+import React from "react";
+import {
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Row,
+} from "reactstrap";
+import "./RequestTalent.scss";
+import { useSelector } from "react-redux";
+import { IRootState } from "../../_reducers";
+import { axiosInstance } from "../../util/axiosConfig";
+import emailjs from "emailjs-com";
 
 /**
  * @function RequestTalent
@@ -12,157 +21,177 @@ import emailjs from 'emailjs-com';
  * when the button is clicked.
  */
 const RequestTalent: React.FC = () => {
+  const [show, setShow] = React.useState(false);
+  const toggle = () => setShow(!show);
 
-    const [show, setShow] = React.useState(false);
-    const toggle = () => setShow(!show);
-   
+  /**
+   * sets clientEmail equal to the email of the client that is logged in
+   */
+  let clientEmail = useSelector((state: IRootState) => {
+    return `${state.userState.user?.email}`;
+  });
 
+  /**
+   * @Function requestTalentFormSubmit
+   * Send form data to the backend to request more talent
+   *
+   * @param event
+   * Collecting information from the form
+   */
 
-    /**
-     * sets clientEmail equal to the email of the client that is logged in
-     */
-    let clientEmail = useSelector((state: IRootState) => {
-        return `${state.userState.user?.email}`
-    });
-    let clientFirstName = useSelector((state: IRootState) => {
-        return `${state.userState.user?.firstName}`
-    })
-    let clientLastName = useSelector((state: IRootState) => {
-        return `${state.userState.user?.lastName}`
-    });
+  const requestTalentFormSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    const email = clientEmail;
+    const message = event.currentTarget["message"].value;
+    const requestType = "TALENT";
 
-
-    /**
-       * @Function requestTalentFormSubmit
-       * Send form data to the backend to request more talent
-       * 
-       * @param event 
-       * Collecting information from the form
-       */
-    
-    
-    const requestTalentFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const email = clientEmail;
-        const message = event.currentTarget["message"].value;
-        const requestType = "TALENT";
-        
-        try {
-            (await axiosInstance()).post(`intervention/`, {
-                clientEmail: email,
-                firstName: clientFirstName,
-                lastName: clientLastName,
-                message: message,
-                requestId: 0,
-                requestType: requestType,
-                status: "PENDING",
-            }).then((response) => {
-                const data = response
-                console.log("post intervention ", data)
-                console.log("data: ", data.config.data)
-
-
-            })
-
-
-        }
-        catch (error) {
-            return false;
-        }
-        toggle();
-
-        const reqReceived = document.getElementById("talentRequested");
-        if (reqReceived) (reqReceived as HTMLElement).innerText = "Request Received!";
-
-        return true;
-    }
-
-
-
-    var adminList:Number[] = [];
-    var adminEmailList:String[] = [];
-    var ClientId:Number;
-    var Message:String;
-    async function NoticeAdmin() {
-        Message = (document.getElementById("message") as HTMLInputElement).value;
-        console.log("You get a notification from Client: ", '\n', " Message: ", Message);
-        (await axiosInstance()).get(`client/email/${clientEmail}`).then((response) => {
-            const data = response
-            console.log("clientinfo ", data)
-            ClientId = data.data.clientId;
-            
+    try {
+      (await axiosInstance())
+        .post(`intervention/`, {
+          clientEmail: email,
+          message: message,
+          requestId: 0,
+          requestType: requestType,
+          status: "PENDING",
+        })
+        .then((response) => {
+          const data = response;
+          console.log("post intervention ", data);
+          console.log("data: ", data.config.data);
         });
-       
-        
-        (await axiosInstance()).get(`admin/`).then((response) => {
-            const data = response
-            for (let i = 0; i < data.data.length; i++) {
-                console.log("adminEmail: ", data.data[i].email);
-                adminEmailList.push(data.data[i].email)
-                adminList.push(data.data[i].adminId)
+    } catch (error) {
+      return false;
+    }
+    toggle();
+
+    const reqReceived = document.getElementById("talentRequested");
+    if (reqReceived)
+      (reqReceived as HTMLElement).innerText = "Request Received!";
+
+    return true;
+  };
+
+  var adminList: Number[] = [];
+  var adminEmailList: String[] = [];
+  var ClientId: Number;
+  var Message: String;
+  async function NoticeAdmin() {
+    Message = (document.getElementById("message") as HTMLInputElement).value;
+    console.log(
+      "You get a notification from Client: ",
+      "\n",
+      " Message: ",
+      Message
+    );
+    (await axiosInstance())
+      .get(`client/email/${clientEmail}`)
+      .then((response) => {
+        const data = response;
+        console.log("clientinfo ", data);
+        ClientId = data.data.clientId;
+      });
+
+    (await axiosInstance()).get(`admin/`).then((response) => {
+      const data = response;
+      for (let i = 0; i < data.data.length; i++) {
+        console.log("adminEmail: ", data.data[i].email);
+        adminEmailList.push(data.data[i].email);
+        adminList.push(data.data[i].adminId);
+      }
+
+      for (let email of adminEmailList) {
+        console.log(adminList);
+        let mailOptions = {
+          adminEmail: email,
+          message: Message,
+        };
+        emailjs
+          .send(
+            "service_780easr",
+            "template_1co3ggw",
+            mailOptions,
+            "user_qNzha4WnrQ4xZeolBYZkl"
+          )
+          .then(
+            (result) => {
+              console.log(result.text);
+            },
+            (error) => {
+              console.log(error.text);
             }
+          );
+      }
+      postRequestMsg();
+    });
+  }
 
-            for (let email of adminEmailList){
-                console.log(adminList);
-                let mailOptions = {
-                    firstName: clientFirstName,
-                    lastName: clientLastName,
-                    adminEmail: email,
-                    message: Message
-                };
-                emailjs.send('service_780easr', 'template_1co3ggw', mailOptions, 'user_qNzha4WnrQ4xZeolBYZkl')
-                    .then((result) => {
-                        console.log(result.text);
-                    }, (error) => {
-                        console.log(error.text);
-                    });     
-            };
-            postRequestMsg();
+  async function postRequestMsg() {
+    Message = (document.getElementById("message") as HTMLInputElement).value;
+    for (let adminId2 of adminList) {
+      (await axiosInstance())
+        .post(`msg/client`, {
+          adminId: adminId2,
+          clientId: ClientId,
+          message: Message,
+        })
+        .then((res) => {
+          console.log(res);
         });
+      console.log("idminId", adminId2);
+      console.log("clientId", ClientId);
+      console.log("imessage", Message);
     }
-    
-    async function postRequestMsg() {
-        Message = (document.getElementById("message") as HTMLInputElement).value;
-        for (let adminId2 of adminList){
-            (await axiosInstance()).post(`msg/client`,{
-                adminId : adminId2,
-                clientId: ClientId,
-                message: Message
-            }).then((res)=>{
-                console.log(res);
-            });
-            console.log("idminId", adminId2)
-            console.log("clientId", ClientId)
-            console.log("imessage", Message);
-        }
-   
-}
+  }
 
-return (
+  return (
     <>
-        <Row className="row justify-content-center" style={{ marginTop: "300px" }}>
-            <button onClick={toggle} className="intervention-button">Request More Talent</button>
-        </Row>
-        <Modal isOpen={show} toggle={toggle}>
-            <ModalHeader toggle={toggle}>
-                <h4>Request More Talent</h4>
-            </ModalHeader>
-            <ModalBody>
-                <Form onSubmit={(event: React.FormEvent<HTMLFormElement>) => requestTalentFormSubmit(event)}>
-                    <FormGroup>
-                        <Label for="message" className="talentTextAreaLabel">Message:</Label>
-                        <Input type="textarea" name="message" id="message" className="talentTextAreaInput" placeholder="Please enter important information regarding the talent you require"></Input>
-                        <input type="submit" value="Submit" className="talentSubmit" onClick={NoticeAdmin} ></input>
-                    </FormGroup>
-                </Form>
-            </ModalBody>
-        </Modal>
-        <br />
-        <Row className="row justify-content-center">
-            <h4 id="talentRequested"></h4>
-        </Row>
+      <Row
+        className="row justify-content-center"
+        style={{ marginTop: "300px" }}
+      >
+        <button onClick={toggle} className="intervention-button">
+          Request More Talent
+        </button>
+      </Row>
+      <Modal isOpen={show} toggle={toggle}>
+        <ModalHeader toggle={toggle}>
+          <h4>Request More Talent</h4>
+        </ModalHeader>
+        <ModalBody>
+          <Form
+            onSubmit={(event: React.FormEvent<HTMLFormElement>) =>
+              requestTalentFormSubmit(event)
+            }
+          >
+            <FormGroup>
+              <Label for="message" className="talentTextAreaLabel">
+                Message:
+              </Label>
+              <Input
+                type="textarea"
+                name="message"
+                id="message"
+                className="talentTextAreaInput"
+                placeholder="Please enter important information regarding the talent you require"
+              ></Input>
+              <input
+                type="submit"
+                value="Submit"
+                className="talentSubmit"
+                onClick={NoticeAdmin}
+              ></input>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+      </Modal>
+      <br />
+      <Row className="row justify-content-center">
+        <h4 id="talentRequested"></h4>
+      </Row>
     </>
-);
-}
+  );
+};
 
 export default RequestTalent;
